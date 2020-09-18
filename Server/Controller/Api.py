fdb403038068a27cb0f6fa154aef149c387a4a86
eXhumer <eXhumer@member.fsf.org>
2020-09-18 12:12:59 -0600

    Interpret scripts as Python 3 /w utf-8 encoding

--- Server/Controller/Api.py
+++ Server/Controller/Api.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import json
 from nut import Status
 from nut import Nsps
--- nut/Usb.py
+++ nut/Usb.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Copyright (c) 2018 Blake Warner
 Copyright (c) 2017-2018 Adubbz
