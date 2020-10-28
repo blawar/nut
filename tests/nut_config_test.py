dbb4aa0ef4c84144aaf75b1711b3d4201194c2ca
introkun <introkun@gmail.com>
2020-10-28 23:59:39 +0300

    Added config set test

--- tests/nut_config_test.py
+++ tests/nut_config_test.py
@@ -12,6 +12,18 @@ class NutConfigTest(unittest.TestCase):
     def test_default_language(self):
         self.assertEqual(config.language, 'en')
 
+    def test_set_paths(self):
+        paths = config.Paths()
+        server = config.Server()
+        j = {}
+        config.set(j, ['paths', 'scan'], paths.scan)
+        config.set(j, ['server', 'hostname'], server.hostname)
+        config.set(j, ['server', 'port'], server.port)
+
+        self.assertEqual(j['paths']['scan'], paths.scan)
+        self.assertEqual(j['server']['hostname'], server.hostname)
+        self.assertEqual(j['server']['port'], server.port)
+
 class NutConfigServerTest(unittest.TestCase):
     def test_default_hostname(self):
         self.assertEqual(config.server.hostname, '0.0.0.0')
