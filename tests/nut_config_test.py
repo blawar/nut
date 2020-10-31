6bfbd9c2353e0e4865802cb8c5066f4c2d635b56
Sergey Gradovich <introkun@gmail.com>
2020-11-01 00:11:16 +0300

    Added tests for config.save method (#204)

--- tests/nut_config_test.py
+++ tests/nut_config_test.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
+import json
+import os
 import unittest
-from importlib import reload
 
-from nut_impl import config
 from pyfakefs.fake_filesystem_unittest import TestCase
+from nut_impl import config
+
+
+def _get_default_config_object():
+    return {'paths': {'scan': ['.']}, 'server': {'hostname': '0.0.0.0', 'port': 9000}}
+
+def _get_default_config_path():
+    return 'conf/nut.conf'
 
 
 class NutConfigTest(TestCase):
@@ -30,20 +37,20 @@ class NutConfigTest(TestCase):
         self.assertEqual(j['server']['hostname'], server.hostname)
         self.assertEqual(j['server']['port'], server.port)
 
-    def test_getGdriveCredentialsFile_without_files(self):
+    def test_get_gdrive_credentials_file_without_files(self):
         self.assertIsNone(config.getGdriveCredentialsFile())
 
-    def test_getGdriveCredentialsFile_with_credentials_file(self):
+    def test_get_gdrive_credentials_file_with_credentials_file(self):
         self.fs.create_file('credentials.json')
         self.assertIsNotNone(config.getGdriveCredentialsFile())
 
-    def test_getGdriveCredentialsFile_with_conf_credentials_file(self):
+    def test_get_gdrive_credentials_file_with_conf_credentials_file(self):
         self.fs.create_file('conf/credentials.json')
         self.assertIsNotNone(config.getGdriveCredentialsFile())
 
     def test_load_non_existing_file(self):
         with self.assertRaises(OSError):
-            config.load('conf/nut.conf')
+            config.load(_get_default_config_path())
         self.assertListEqual(config.paths.scan, ['.'])
         self.assertEqual(config.server.hostname, '0.0.0.0')
         self.assertEqual(config.server.port, 9000)
@@ -66,6 +73,47 @@ class NutConfigTest(TestCase):
         self.assertListEqual(config.paths.scan, ['./'])
         self.assertEqual(config.server.hostname, '127.0.0.1')
         self.assertEqual(config.server.port, 9005)
+
+    def __compare_config_in_file_with_object(self, conf_file, obj):
+        self.assertTrue(os.path.exists(conf_file))
+        with open(conf_file, 'r', encoding="utf8") as f:
+            obj_from_file = json.load(f)
+            self.assertEqual(obj_from_file, obj)
+
+    def test_save_default_config_with_explicit_config_path(self):
+        conf_file = _get_default_config_path()
+        self.assertFalse(os.path.exists(conf_file))
+        with self.assertRaises(OSError):
+            config.save(conf_file)
+        self.fs.create_file(conf_file)
+        config.save(conf_file)
+        self.__compare_config_in_file_with_object(conf_file, _get_default_config_object())
+
+    def test_save_default_config_with_implicit_config_path(self):
+        conf_file = _get_default_config_path()
+        self.assertFalse(os.path.exists(conf_file))
+        with self.assertRaises(OSError):
+            config.save()
+        self.fs.create_file(conf_file)
+        config.save()
+        self.__compare_config_in_file_with_object(conf_file, _get_default_config_object())
+
+    def test_save_custom_config_with_explicit_config_path(self):
+        conf_file = _get_default_config_path()
+        self.fs.create_file(conf_file)
+        nsps_folder = "/Users/user1/nsps"
+        server_hostname = "127.0.0.1"
+        server_port = 9001
+
+        config.paths.scan = [nsps_folder]
+        config.server.hostname = server_hostname
+        config.server.port = server_port
+
+        object_to_compare = {'paths': {'scan': [nsps_folder]}, 'server': {\
+            'hostname': server_hostname, 'port': server_port}}
+
+        config.save(conf_file)
+        self.__compare_config_in_file_with_object(conf_file, object_to_compare)
 
 
 class NutConfigServerTest(TestCase):
