0027650c1ad2e06688eeccd440d4476fb919938a
Sergey Gradovich <introkun@gmail.com>
2020-11-17 18:23:28 +0300

    Fixed slow table update (fixes #215) (#226)
    * added custom sorting model

--- gui/panes/files.py
+++ gui/panes/files.py
@@ -1,42 +1,27 @@
-import os
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget, QVBoxLayout, QTableWidget, QTableWidgetItem
-from PyQt5.QtGui import QIcon
+
 from PyQt5 import QtWidgets
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QTableView
+
 from nut import Nsps
+from gui.table_model import TableModel
 
-class Files(QTableWidget):
+
+class Files(QTableView):
 	def __init__(self):
-		super(QTableWidget, self).__init__()
-		self.setColumnCount(4)
-
-		headers = [QTableWidgetItem("File"), QTableWidgetItem("Title ID"), QTableWidgetItem("Type"), QTableWidgetItem("Size")]
-
-		i = 0
-		for h in headers:
-			self.setHorizontalHeaderItem(i, h)
-			i = i + 1
+		super(QTableView, self).__init__()
+		self.model = TableModel(self)
+		self.setModel(self.model)
 
 		header = self.horizontalHeader()
-		i = 0
-		for h in headers:
-			header.setSectionResizeMode(i, QtWidgets.QHeaderView.Stretch if i == 0 else QtWidgets.QHeaderView.ResizeToContents)
-			i = i + 1
+		header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+
+		self.setSortingEnabled(True)
+		self.sortByColumn(0, Qt.AscendingOrder)
 
 		self.refresh()
 
 	def refresh(self):
-		self.setRowCount(len(Nsps.files))
-		i = 0
-		for k, f in Nsps.files.items():
-			title = f.title()
-			if f.path.endswith('.nsx') or not title.isActive(True):
-				continue
-
-			self.setItem(i, 0, QTableWidgetItem(os.path.basename(f.path)))
-			self.setItem(i, 1, QTableWidgetItem(str(f.titleId)))
-			self.setItem(i, 2, QTableWidgetItem("UPD" if title.isUpdate else ("DLC" if title.isDLC else "BASE")))
-			self.setItem(i, 3, QTableWidgetItem(str(f.fileSize or title.size)))
-			i = i + 1
-
-		self.setRowCount(i)
+		self.setUpdatesEnabled(False)
+		self.model.update(Nsps.files)
+		self.setUpdatesEnabled(True)
