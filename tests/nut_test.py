d660fc11a9d8b08370dfe6e7db179da3829a9ed1
introkun <introkun@gmail.com>
2020-11-27 22:56:15 +0300

    fixed constant

--- tests/nut_test.py
+++ tests/nut_test.py
@@ -2,7 +2,7 @@ import unittest
 
 import nut
 
-REGION_LANG_TEST_DATA = [{'region': 'US', 'lang': 'en', 'expected_score': 119}, \
+_REGION_LANG_TEST_DATA = [{'region': 'US', 'lang': 'en', 'expected_score': 119}, \
 	{'region': 'FR', 'lang': 'fr', 'expected_score': 118}, \
 	{'region': 'JA', 'lang': 'jp', 'expected_score': 110}, \
 	{'region': 'ES', 'lang': 'es', 'expected_score': 116}]
@@ -11,7 +11,7 @@ class NutTest(unittest.TestCase):
 	"""Tests for nut/__init__.py
 	"""
 	def test_region_language(self):
-		for data in REGION_LANG_TEST_DATA:
+		for data in _REGION_LANG_TEST_DATA:
 			lang = nut.RegionLanguage(data['region'], data['lang'], data['region'], data['lang'])
 			self.assertEqual(lang.region, data['region'])
 			self.assertEqual(lang.language, data['lang'])
@@ -21,7 +21,7 @@ class NutTest(unittest.TestCase):
 			self.assertEqual(lang.score, data['expected_score'])
 
 	def test_region_language_with_preffered(self):
-		data = REGION_LANG_TEST_DATA[0]
+		data = _REGION_LANG_TEST_DATA[0]
 		PREFERRED_REGION = 'RU'
 		PREFERRED_LANGUAGE = 'ru'
 		self.assertNotEqual(data['region'], PREFERRED_REGION)
@@ -34,7 +34,7 @@ class NutTest(unittest.TestCase):
 		self.assertEqual(lang.score, 9)
 
 	def test_region_language_less(self):
-		data = REGION_LANG_TEST_DATA[0]
+		data = _REGION_LANG_TEST_DATA[0]
 		PREFERRED_REGION = 'RU'
 		PREFERRED_LANGUAGE = 'ru'
 		lang1 = nut.RegionLanguage(data['region'], data['lang'], data['region'], data['lang'])
