c2259fe7ebbf7e3fe80a08e31818156522de6700
introkun <introkun@gmail.com>
2021-01-10 00:13:50 +0300

    added more gui tests

--- tests/gui_app_test.py
+++ tests/gui_app_test.py
@@ -1,10 +1,19 @@
+import sys
+
+from PyQt5.QtWidgets import (QApplication)
+from PyQt5.QtTest import QTest
+
 import unittest
+
+from gui.app import App
+
+app = QApplication(sys.argv)
 
 class GuiAppTest(unittest.TestCase):
 	"""Tests for gui/app.py
 	"""
 	def setUp(self):
-		pass
+		self.form = App()
 
 	def test_run(self):
-		pass
+		self.assertEqual(self.form.title, 'NUT 3.0')
