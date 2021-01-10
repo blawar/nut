9f3dcc4939e5e5c79b9c1518159ddb7bb1d39cd6
introkun <introkun@gmail.com>
2021-01-10 21:23:10 +0300

    added more gui tests

--- tests-gui/gui_app_test.py
+++ tests-gui/gui_app_test.py
@@ -1,20 +1,31 @@
 import sys
-
-from PyQt5.QtWidgets import (QApplication)
-from PyQt5.QtTest import QTest
-
 import unittest
 
-from gui.app import App
+from PyQt5.QtTest import QTest
+from PyQt5.QtWidgets import QApplication, QPushButton, QLineEdit
 
-app = QApplication(sys.argv)
+from gui.app import App
 
 class GuiAppTest(unittest.TestCase):
 	"""Tests for gui/app.py
 	"""
 	def setUp(self):
+		self.app = QApplication(sys.argv)
 		self.form = App()
 
 	def test_run(self):
 		self.assertEqual(self.form.title, 'NUT 3.0')
 		self.form.header.scan.click()
+		self.form.tabs.tabs.setCurrentIndex(0) # files
+		self.form.tabs.tabs.setCurrentIndex(1) # filters
+		self.form.tabs.tabs.setCurrentIndex(2) # save paths
+		self.form.tabs.tabs.setCurrentIndex(3) # local scan paths
+		self.form.tabs.tabs.setCurrentIndex(4) # remote scan paths
+		self.form.tabs.tabs.setCurrentIndex(5) # users
+		self.form.tabs.tabs.setCurrentIndex(6) # options
+
+		scan_path_widget = self.form.tabs.tabs.widget(3) # local scan paths
+		print(scan_path_widget.metaObject().className())
+		scan_edit = scan_path_widget.findChildren(QLineEdit)[0]
+		add_button = scan_path_widget.findChildren(QPushButton)
+		self.assertIsNotNone(add_button)
