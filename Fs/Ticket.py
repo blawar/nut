893c3c083cb83bde594c20796152c59f13b58621
blawar <blake@null3d.com>
2021-07-02 14:21:14 -0400

    fixed crash when ticket contains invalid type

--- Fs/Ticket.py
+++ Fs/Ticket.py
@@ -57,6 +57,8 @@ class Ticket(File):
 		self.masterKeyRevision = self.readInt8()
 
 	def seekStart(self, offset):
+		if self.signatureType not in self.signatureSizes:
+			raise IOError('invalid signature type: ' + str(self.signatureType))
 		self.seek(0x4 + self.signatureSizes[self.signatureType] + self.signaturePadding + offset)
 
 	def getSignatureType(self):
@@ -189,7 +191,11 @@ class Ticket(File):
 	def printInfo(self, maxDepth=3, indent=0):
 		tabs = '\t' * indent
 
-		rightsId = format(self.getRightsId(), 'X').zfill(32)
+		try:
+			rightsId = format(self.getRightsId(), 'X').zfill(32)
+		except:
+			Print.info('\n%sInvalid Ticket\n' % (tabs))
+			return
 		titleId = rightsId[0:16]
 		titleKey = format(self.getTitleKeyBlock(), 'X').zfill(32)
 
