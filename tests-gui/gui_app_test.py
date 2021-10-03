d78df1a7716b1ecd103a0739e50045a9769d2c7e
introkun <introkun@gmail.com>
2021-10-03 16:21:03 +0300

    added more tests

--- tests-gui/gui_app_test.py
+++ tests-gui/gui_app_test.py
@@ -6,6 +6,9 @@ import logging
 from PyQt5.QtWidgets import (QApplication, QPushButton, QLineEdit, QSlider)
 
 from gui.app import App
+from gui.panes.options import Threads, Compress
+
+from nut import Config
 
 OPTIONS_TAB_INDEX = 6
 
@@ -40,9 +43,17 @@ class GuiAppTest(unittest.TestCase):
 		tabs = self.form.tabs.tabs
 		tabs.setCurrentIndex(OPTIONS_TAB_INDEX)
 		sliders = tabs.findChildren(QSlider)
-		threads_slider: QSlider = sliders[0]
-		compression_slider: QSlider = sliders[1]
+		threads_slider: Threads = sliders[0]
+		compression_slider: Compress = sliders[1]
 		self.assertEqual(threads_slider.minimum(), 1)
 		self.assertEqual(threads_slider.maximum(), 8)
 		self.assertEqual(compression_slider.minimum(), 0)
 		self.assertEqual(compression_slider.maximum(), 22)
+
+		threads_slider.setValue(3)
+		threads_slider.save()
+		self.assertEqual(Config.threads, threads_slider.value())
+
+		compression_slider.setValue(10)
+		compression_slider.save()
+		self.assertEqual(Config.compression.level, compression_slider.value())
